a203615af608f22e32fd5e6bf1afe68aaa5aa9a3
Sebastian Rojo <arpagon@gmail.com>
2024-02-24 18:47:37 -0500

    Add Vercel deployment and Vocode logo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['vercel.com'],
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,37 +12,58 @@ const VocodeAppDynamic = dynamic(() => import('@/components/VocodeApp.client'), 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://docs.vocode.dev?utm_source=vocode-next-vercel-template&utm_medium=vocode-next-vercel-template&utm_campaign=vocode-next-vercel-template"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vocode.svg"
-              alt="Vocode Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
+      <div className="flex justify-end">
+            <a
+              href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fnext.js%2Ftree%2Fcanary%2Fexamples%2Fhello-world"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://vercel.com/button"
+                alt="Deploy with Vercel"
+              />
+            </a>
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              href="https://docs.vocode.dev?utm_source=vocode-next-vercel-template&utm_medium=vocode-next-vercel-template&utm_campaign=vocode-next-vercel-template"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <Image
+                src="/vocode.svg"
+                alt="Vocode Logo"
+                className="dark:invert"
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-900 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/vocode.svg"
           alt="Vocode Logo"
+          width={280}
+          height={37}
+          priority
+        />
+        <a className="text-2xl px-6">/</a>
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next Logo"
           width={180}
           height={37}
           priority
         />
       </div>
+      <p className="text-lg text-center font-semibold dark:text-white">
+        The fastest way to build Voice AI application with 
+        <a href="https://vocode.dev" target="_blank" rel="noopener noreferrer" className="text-blue-800 hover:underline dark:text-blue-300 dark:hover:underline"> Vocode</a> and 
+        <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="text-blue-800 hover:underline dark:text-blue-300 dark:hover:underline"> Next.js</a>
+      </p>
       <div className="flex flex-col items-center justify-center py-4">
         <VocodeAppDynamic defaultBackendUrl="ws://localhost:19002/conversation" isInputEditable={false} />
       </div>
