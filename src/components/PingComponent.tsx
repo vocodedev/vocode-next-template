93f2c61054a5e64fa074591ada47a96278a80fe1
Sebastian Rojo <arpagon@gmail.com>
2024-02-29 09:10:59 -0500

    Add "use client" import statements to AudioVisualizer and PingComponent

--- src/components/PingComponent.tsx
+++ src/components/PingComponent.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState, useRef } from 'react';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 import { Button } from "@/components/ui/button"
